adfbbfd1e292f1366d595e152700a4cc77e90e9d
math-vieira <matheusvieira9153@gmail.com>
2023-01-13 19:44:54 -0300

    refactor:  add function to create request url

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,2 +1,11 @@
-module.exports.chart = "https://api.deezer.com/chart" //https://api.deezer.com/chart/0/tracks?limit=1&index=2
-module.exports.search = "https://api.deezer.com/search?q=" //https://api.deezer.com/search?q=eminem&type=track&limit=1&index=2
+const baseChartUrl = "https://api.deezer.com/chart"; //https://api.deezer.com/chart/0/tracks?limit=10&index=1
+const baseSearchUrl = "https://api.deezer.com/search?q="; //https://api.deezer.com/search?q=eminem&type=track&limit=1&index=2
+
+module.exports.createChartUrl = ({ type, limit, index }) => {
+    return `${baseChartUrl}/0/${type}?limit=${limit}&index=${index}`;
+};
+
+module.exports.createSearchUrl = ({ type, limit, index, query }) => {
+    return `${baseSearchUrl}${query}&type=${type}&limit=${limit}&index=${index}`;
+};
+//https://api.deezer.com/search?q=eminem&type=track&limit=1&index=2
